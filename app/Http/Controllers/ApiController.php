e4b987bab1a037c19236bb3c3076563c59c55224
Giovani <giovanimouradev@gmail.com>
2021-04-10 14:50:32 -0300

    Rotas criadas

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,4 +7,20 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     //
+
+    public function creaLista(){
+
+    }
+    public function lerTodos(){
+        
+    }
+    public function exbirTodos(){
+        
+    }
+    public function updateLista(){
+        
+    }
+    public function deleteLista(){
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController; 
 
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::get("/ping",function(){
     return [
         "pong" => true,
@@ -15,3 +16,26 @@ Route::get("/ping",function(){
     ];
 
 });
+
+
+//Crud de lista
+
+//create = metodos para criar tarefas
+//Read = metdos pra ler umas ou todas as tarefas
+//Update = metodospara atualizar uma tarefa
+//Delete  = metodos para deletar uma tarefa
+
+
+//POST ->Inserir tarefas no sistemas [TODO]
+//GET ->ler todas as tarefas do sistema[TODOS]
+//GET ->ler todas as tarefas do sistema[TODOS/2]
+//PUT ->atualizar tarefa no  [TODOS/2]
+//DELETE ->atualizar uma tarefa no sistema [TODOS/2]
+
+
+Route::post('/todo',[ApiController::class, 'creaLista']);//crear
+Route::get('/todo',[ApiController::class, 'lerTodos']); //exibir
+Route::get('/todo/{id}',[ApiController::class, 'exbirTodos']); //ler um so
+Route::put('/todo/{id}',[ApiController::class, 'updateLista']); //atualizar
+Route::delete('/todo/{id}',[ApiController::class, 'deleteLista']); //deltar
+        
